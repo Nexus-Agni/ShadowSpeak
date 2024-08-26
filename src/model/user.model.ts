d53538f84406cf2e1be3bea3603f3877165a7959
Nexus-Agni <agnibhachakraborty11@gmail.com>
2024-08-26 14:58:46 +0530

    feat: Add user model and message schema

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,4 +1,4 @@
-import mongoose,{Schema, Document} from "mongoose";
+import mongoose,{Schema, Document, Mongoose} from "mongoose";
 
 export interface Message extends Document {
     content : string; //string with small "s" comes from typescript
@@ -37,7 +37,36 @@ const UserSchema : Schema<User> = new mongoose.Schema({
     }, 
     email : {
         type : String,
-        required : [true, "Please use a valid email address"],
-        
-    }
+        required : [true, "Email is required"],
+        trim : true,
+        unique : true,
+        match : [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Please enter a valid email address"]
+    },
+    password : {
+        type : String,
+        required : [true, "Password is a mandatory field"],
+        trim : true
+    },
+    verifyCode : {
+        type : String,
+        required : [true, "Verify code is required"],
+        trim : true
+    },
+    verifyCodeExpiry : {
+        type : Date,
+        required : [true, "Verify Code expiry is required"]
+    },
+    isVerified : {
+        type : Boolean,
+        default : false
+    },
+    isAcceptingMessages : {
+        type : Boolean,
+        default : false
+    },
+    messages : [MessageSchema]
 })
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("User", UserSchema))
+
+export default UserModel;   
