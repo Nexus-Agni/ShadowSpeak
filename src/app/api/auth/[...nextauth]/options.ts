0bf796bc2ff32eb11c0df043c55aefe2d06f7e2e
Nexus-Agni <agnibhachakraborty11@gmail.com>
2024-09-03 06:59:05 +0530

    feat: Update user authentication with next-auth
    This commit updates the user authentication in the project by adding next-auth. Dependencies have been updated to include next-auth version 4.0.0. The .gitignore file has been updated to include local environment files. The sign-up route has been modified to handle existing users and send a verification email. Additionally, a new user model and message schema have been added to the project.
    
    This commit addresses the need to enhance user authentication and implement user functionality and message storage in the application.

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -19,10 +19,10 @@ export const authOptions : NextAuthOptions = {
                 // Algorithm: 
                 // 1. finding the user from the database
                 // 2. if user not present and if user is not verified, throw error
-
+                // 3. Check password -> if correct return user, else throw error.
 
                 try {
-                    const user = UserModel.findOne({
+                    const user = await UserModel.findOne({
                         $or : [
                             {username : credentials.identifier.username},
                             {email : credentials.identifier.email}
@@ -34,11 +34,43 @@ export const authOptions : NextAuthOptions = {
                     if (!user.isVerified) {
                         throw new Error("Please verify your account before logging in")
                     }
-
+                    const isPasswordCorrect = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    )
+                    if (isPasswordCorrect) {
+                        return user;
+                    } else {
+                        throw new Error("Incorrect Password")
+                    }
                 } catch (err : any) {
                     throw new Error(err);
                 }
             },
         }),
-    ]
+    ],
+    callbacks : {
+        async jwt({ token, user }) {
+            token._id = user._id?.toString();
+            token.isAcceptingMessage =user.isAcceptingMessage;
+            token.isVerified = user.isVerifed;
+            token.username = user.username;
+            return token
+        },
+        async session({ session, token }) {
+            session.user._id = token._id;
+            session.user.isVerifed = token.isVerified;
+            session.user.isAcceptingMessage = token.isAcceptingMessage;
+            session.user.username = token.username;
+            return session
+        },
+    },
+    pages : {
+        signIn : '/sign-in'
+    },
+    secret : process.env.SECRET_KEY,
+    session : {
+        strategy : "jwt"
+    },
+
 }
